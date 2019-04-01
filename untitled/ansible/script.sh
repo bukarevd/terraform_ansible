1fbc9c204d078cf856da7033800e299daea50573
bukarevd <bukarevd@gmail.com>
2019-04-02 00:26:05 +0300

    set permision for ssh key

--- untitled/ansible/script.sh
+++ untitled/ansible/script.sh
@@ -4,5 +4,6 @@ sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt-get install ansible -y
+chmod 400 ~/.ssh/NVirginia_home.pem
 mkdir ansible
 sudo echo "--------------END-------------------"
